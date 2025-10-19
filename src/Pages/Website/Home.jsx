cf7f9538476c0b6a112a2823c1ed5667dfa272d5
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-10-18 22:40:43 -0300

    Complete Home

--- src/Pages/Website/Home.jsx
+++ src/Pages/Website/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import Hero from "../../Components/shared/Hero.jsx";
 import { Link } from "react-router";
 import Style from "../../Styles/pages/Home.module.css";
@@ -30,32 +30,63 @@ const list = [
   },
 ];
 const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        // const response = await fetch("/api/products");
+        // const data = await response.json();
+        const data = await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(list);
+          }, 1000);
+        });
+        setProducts(data);
+        setCategories([...new Set(data.map((item) => item.category))]);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <main className={Style.homeMain}>
       <Hero title="Swimming Vives" />
-      <section className={Style.categoryContent}>
-        <ul>
+      <section className={Style.content}>
+        <article className={Style.products}>
           <h2>Mas Vendidos</h2>
-          {list.map((item) => (
-            <li key={item.id}>
-              <figure>
-                <img src={item.img} alt={item.name} />
-              </figure>
-              <article>
-                <h3>{item.name}</h3>
-                <p>{item.category}</p>
-              </article>
-            </li>
-          ))}
-        </ul>
-        <nav>
+          <ul>
+            {products.map((item) => (
+              <li key={item.id}>
+                <figure>
+                  <img src={item.img} alt={item.name} />
+                </figure>
+                <article>
+                  <h3>{item.name}</h3>
+                  <p>{item.category}</p>
+                </article>
+              </li>
+            ))}
+          </ul>
+        </article>
+        <article className={Style.categories}>
           <h2>Categorias</h2>
-          {list.map((item) => (
-            <Link key={item.id}>
-              <p>{item.category}</p>
-            </Link>
-          ))}
-        </nav>
+          <nav>
+            {categories.map((item, index) => (
+              <Link
+                key={index}
+                to={`/productos?${new URLSearchParams({
+                  category: item.toLowerCase(),
+                }).toString()}`}
+              >
+                {item}
+              </Link>
+            ))}
+          </nav>
+        </article>
       </section>
     </main>
   );
